52618fb4507c9c7a370433035e0299104580ee3b
Matvey <howtomatben@gmail.com>
2025-10-05 13:31:01 +0200

    player model 3.0

--- Assets/Project/Code/Player/PlayerShooter.cs
+++ Assets/Project/Code/Player/PlayerShooter.cs
@@ -5,10 +5,33 @@ public class PlayerShooter : MonoBehaviour
 {
     [SerializeField] private ProjectileShooter projectileShooter;
     [SerializeField] private PlayerInputHub playerInputHub;
+    [SerializeField] private Animator animator;
+    private bool isHoldingAttack = false;
+    private bool isShooting = false;
+
+    public void OnShootAnimationEnd()
+    {
+        projectileShooter.isFiring = true;
+        Debug.Log("Shooting");
+        // Example: reset shooting flag
+    }
 
     private void Update()
     {
-        projectileShooter.isFiring = playerInputHub && playerInputHub.AttackHeld;
+        isHoldingAttack = playerInputHub && playerInputHub.AttackHeld;
+        if (projectileShooter.CanFire)
+        {
+            if (!isShooting && isHoldingAttack && !projectileShooter.isFiring)
+            {
+                animator.SetTrigger("Shoot");
+                isShooting =  true;
+            } 
+        }
+        else
+        {
+            projectileShooter.isFiring = false;
+            isShooting  = false;
+        }
 
     }
 }
